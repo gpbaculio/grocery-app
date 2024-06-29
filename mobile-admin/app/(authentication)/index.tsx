6f8359741ab32c338e46f790ab58531b2aea1180
Glendon Philipp Baculio <gpbaculio@gmail.com>
2024-06-29 11:27:51 +0800

    test icon

--- mobile-admin/app/(authentication)/index.tsx
+++ mobile-admin/app/(authentication)/index.tsx
@@ -1,11 +1,15 @@
-import { View, Text } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 import React from "react";
+import { DynamicText, DynamicView } from "@/components";
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <DynamicView flex={1} variant="centerItems">
+      <DynamicView>
+        <FontAwesome name="cart-plus" size={48} color="green" />
+        <DynamicText mt="S">Login to your account</DynamicText>
+      </DynamicView>
+    </DynamicView>
   );
 };
 
